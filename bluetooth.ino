5703766c8c956e1489e4174971d1d54004e5c3fb
Abhi1code <38901225+Abhi1code@users.noreply.github.com>
2018-10-05 00:27:06 +0530

    Add files via upload

---
+++ bluetooth.ino
@@ -0,0 +1,27 @@
+#include<SoftwareSerial.h>
+SoftwareSerial BT(0,1);
+String readdata;
+void setup()
+{
+  BT.begin(9600);
+  Serial.begin(9600);
+  
+}
+void loop()
+{
+  while (BT.available())
+  {
+    delay(10);
+    char c = BT.read();
+    readdata += c;
+    
+  }
+  if(readdata.length()>0)
+  {
+    Serial.print(readdata);
+    readdata="";
+    
+  }
+  Serial.print("aaaa#");
+}
+
